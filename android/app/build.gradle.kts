25591d1597280eb0d8865a63eccff27cb0cfa17f
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-06-11 22:20:59 -0300

    test: running app on phone

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,15 +8,32 @@ plugins {
 android {
     namespace = "gabriel.graciano.keybook"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+//    ndkVersion = flutter.ndkVersion
+
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_11
+//        targetCompatibility = JavaVersion.VERSION_11
+//    }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
     }
 
+//    kotlinOptions {
+//        jvmTarget = JavaVersion.VERSION_11.toString()
+//    }
+
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
+    }
+
+    dependencies {
+        // ... other dependencies
+        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+        // ...
     }
 
     defaultConfig {
@@ -24,7 +41,7 @@ android {
         applicationId = "gabriel.graciano.keybook"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
