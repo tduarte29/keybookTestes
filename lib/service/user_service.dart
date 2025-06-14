ee3d2bcdfdaf87f190fb6f7ad6adec675b0a4649
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-06-14 17:22:28 -0300

    feat: Organização de arquivos para lidar com o backend

---
+++ lib/service/user_service.dart
@@ -0,0 +1,13 @@
+// class UserService {
+//   static Future<Map<String, dynamic>> getUserProfile() async {
+//     // Busca dados do usuário
+//   }
+//
+//   static Future<void> updateProfile(String userId, String newName, String newEmail) async {
+//     // Atualiza perfil
+//   }
+//
+//   static Future<void> deleteAccount(String userId) async {
+//     // Exclui conta
+//   }
+// }
