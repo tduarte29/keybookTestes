98dc9bdec8e9a6764e831abfe1b945039f4d98ec
Gabriel <135984152+gsgabs@users.noreply.github.com>
2025-06-22 16:02:28 -0300

    refactor: remoção de código comentado e arquivos não usados

--- lib/screens/historic_screen.dart
+++ lib/screens/historic_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// TELA PRINCIPAL
 class HistoricScreen extends StatelessWidget {
   const HistoricScreen({super.key});
 
@@ -26,9 +25,7 @@ class HistoricScreen extends StatelessWidget {
               const SizedBox(height: 16),
               // Histórico de exportações (PDFs)
               GestureDetector(
-                onTap: () {
-                  
-                },
+                onTap: () {},
                 child: Container(
                   width: double.infinity,
                   height: 160,
@@ -56,7 +53,9 @@ class HistoricScreen extends StatelessWidget {
                           child: Text(
                             'Nenhum PDF exportado ainda.',
                             style: GoogleFonts.inter(
-                                color: Colors.white54, fontSize: 14),
+                              color: Colors.white54,
+                              fontSize: 14,
+                            ),
                           ),
                         ),
                       ),
@@ -100,7 +99,9 @@ class HistoricScreen extends StatelessWidget {
                           child: Text(
                             'Nenhuma modificação recente.',
                             style: GoogleFonts.inter(
-                                color: Colors.white54, fontSize: 14),
+                              color: Colors.white54,
+                              fontSize: 14,
+                            ),
                           ),
                         ),
                       ),
